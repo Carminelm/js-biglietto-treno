29a1a6454d4bd35368ac6d6dd8fd6c82a6134f15
Carminelm <carminelm54@gmail.com>
2024-06-04 16:32:36 +0200

    add user dates and km

--- main.js
+++ main.js
@@ -1,13 +1,86 @@
-// collegamento html 
-
-let userKM = document.getElementById ('kilometri');
-let userAge = document.getElementById ('age');
-let userCost = document.getElementById ('results');
-
 // Chiediamo all' utente il numero di km che vuole percorrere
 
-userKM.innerHTML = prompt ('Inserisci i kilometri che vuoi percorrere');
+let userKM = prompt('Inserisci i kilometri che vuoi percorrere');
+
+
+console.log(userKM);
 
 // Chiediamo all'utente la sua eta
 
+let userAge = prompt ('Inserisci la tua età');
+
+console.log(userAge);
+
+// Calcolo costo per kilometro 
+
+let KMcost = userKM * 0.21;
+
+console.log(KMcost);
+
+
+// Prezzi per Età
+
+let trainPrice;
+
+if (userAge > 65) {
+
+    trainPrice = (KMcost * 0.126).toFixed(2)
+}
+
+else if  (userAge < 18) {
+
+    trainPrice = (KMcost * 0.168).toFixed(2)
+}
+
+else  {
+
+    trainPrice = KMcost.toFixed(2)
+}
+
+console.log(trainPrice);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 userAge.innerHTML = prompt ('Inserisci la tua età');
+
+
+
+documen.getElementById('kilometri').innerHTML = userKM
+
+
+
+
+
+
+
+
+
+//calcoli per il  biglieto
+
+let userCost = document.getElementById ('results');
+
+
+
+
+
+// collegamento html 
