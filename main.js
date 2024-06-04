ec1d65f8493856312696933ea6b334fcb7b8f785
Carminelm <carminelm54@gmail.com>
2024-06-04 15:23:11 +0200

    add dates

---
+++ main.js
@@ -0,0 +1,13 @@
+// collegamento html 
+
+let userKM = document.getElementById ('kilometri');
+let userAge = document.getElementById ('age');
+let userCost = document.getElementById ('results');
+
+// Chiediamo all' utente il numero di km che vuole percorrere
+
+userKM.innerHTML = prompt ('Inserisci i kilometri che vuoi percorrere');
+
+// Chiediamo all'utente la sua eta
+
+userAge.innerHTML = prompt ('Inserisci la tua età');
